be86d4b37749bee4b0c209476b136cccdd1a6778
suzuki86 <tsnr0001@gmail.com>
2014-07-18 08:29:38 +0900

    Replace tabs with two spaces #1

--- PeriodicRebuildAllBlogs.pl
+++ PeriodicRebuildAllBlogs.pl
@@ -7,32 +7,32 @@ use MT::WeblogPublisher;
 use base qw( MT::Plugin );
 
 my $plugin = new MT::Plugin({
-	name => 'PeriodicRebuildAllBlogs',
-	key => 'PeriodicRebuildAllBlogs',
-	author_name => 'Suzuki Toshinari',
-	author_link => 'http://suzuki.toshinari.jp/',
-	version => '0.1',
-	description => 'run-periodic-rebuildが実行された時に全てのブログを再構築します。'
+  name => 'PeriodicRebuildAllBlogs',
+  key => 'PeriodicRebuildAllBlogs',
+  author_name => 'Suzuki Toshinari',
+  author_link => 'http://suzuki.toshinari.jp/',
+  version => '0.1',
+  description => 'run-periodic-rebuildが実行された時に全てのブログを再構築します。'
 });
 
 MT->add_plugin($plugin);
 
 MT->add_task(new MT::Task({
-	name => 'PeriodicRebuildAllBlogs',
-	key => 'PeriodicRebuildAllBlogs',
-	frequency => 10,
-	code => \&rebuild_all_blogs
+  name => 'PeriodicRebuildAllBlogs',
+  key => 'PeriodicRebuildAllBlogs',
+  frequency => 10,
+  code => \&rebuild_all_blogs
 }));
 
 sub rebuild_all_blogs{
-	my $rbld = MT::WeblogPublisher->new;
-	my $iter = MT->model('blog')->load_iter();
+  my $rbld = MT::WeblogPublisher->new;
+  my $iter = MT->model('blog')->load_iter();
 
-	while(my $blog = $iter->()){
-		$rbld->rebuild(
-			BlogID => $blog->id
-		);
-	}
+  while(my $blog = $iter->()){
+    $rbld->rebuild(
+      BlogID => $blog->id
+    );
+  }
 }
 
 1;
